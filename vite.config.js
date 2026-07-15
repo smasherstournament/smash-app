95c3271391848a46970d68ab719a6b49dbd96183
smasherstournament <smasherstournament@gmail.com>
2026-07-15 20:45:34 +0530

    Update vite.config.js
    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/peaceproject/', // MUST match your repo name exactly
+  base: '/smash-app/', //  match your repo name exactly
 })
